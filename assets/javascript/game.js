bb7b4cb651d39ec6868fcf98bfc3d78b159fdd18
Amir Ahmed <amir@Amirs-MacBook-Pro.local>
2017-11-07 18:16:56 -0600

    added alerts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,49 @@
-//Start the game when user presses the spacebar
-document.onkeydown = function() {
+window.onload = function() {
 
-//Define an array of songs of songs
-var songs = ["Holy Grail", "Tom Ford", "Young Forever", "Empire State of Mind", "99 Problems", "I Just Wanna Love U", "On to the Next One"];
+	//Start the game when user presses the spacebar
+	var body = document.querySelector("body");
+	document.body.onkeydown = function(e) {
+		if (e.keyCode == 32) {
+		
+			//Ask the user's name
+			var name = prompt("Hi there, what's your name?");
 
-//Define a random song to use in Hangman
-var randomSong = songs[Math.floor(Math.random() * songs.length)];
+			//Greet the user
+			alert("Hi " + name);
+			alert("Can you guess the name of the JAY Z song?");
+			alert("To guess, simply press the letter on the keyboard");
+			alert("Good luck!"); 
 
-//Take song and split up into individual letters
-var arrayrandomSong = randomSong.split("");
+			//Define an array of songs
+			var songs = ["Holy Grail", "Tom Ford", "Young Forever", "Empire State of Mind", "99 Problems", "I Just Wanna Love U", "On to the Next One"];
 
-//Map split song to blanks
-var arrayOfBlanks = arrayrandomSong.map(a=>'_ ');
+			//Define a random song to use in Hangman
+			var randomSong = songs[Math.floor(Math.random() * songs.length)];
 
+			//Take song and split up into individual letters
+			var arrayrandomSong = randomSong.split("");
 
-//Write blanks to the page
-document.getElementById("blanks").innerHTML = arrayOfBlanks;
+			//Convert randomSong to a string???
+			//var stringrandomSong = arrayRandomSong.toString();
 
-};
+			//Map split song to blanks
+			var arrayOfBlanks = arrayrandomSong.map(a=>' _ ');
+			//arrayOfBlanks.toString()replace(',','');
+			arrayOfBlanks.toString().replace(',',' ');
+			
+			//Write blanks to the page
+			document.getElementById("blanks").innerHTML = arrayOfBlanks;
+
+			//Capture the user's input
+			//var guess = 
+
+		}
+		
+		else {
+			alert("Wrong key! Press the spacebar to begin!");
+		}
+		
+		
+	}
+
+}
