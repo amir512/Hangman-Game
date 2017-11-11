b145bcf2942b44dc22532a14ac375df6f7e2a5ec
Amir Ahmed <amir@Amirs-MacBook-Pro.local>
2017-11-11 03:19:28 -0600

    added JS

--- assets/javascript/game_alternative.js
+++ assets/javascript/game_alternative.js
@@ -2,6 +2,9 @@ window.addEventListener("load", hangmanStart);
 
 function hangmanStart() {
 
+
+function createHangmanSong() {
+
 var name //user's name
 var NumberofGuesses = 10; //Number of times a user can guess a letter
 var lettersGuessed = []; //array to hold the letters already guessed
@@ -17,6 +20,7 @@ console.log(arrayrandomSong);
 var stringrandomSongNoCommas = arrayrandomSong.join(""); //Take random song and remove the commas
 console.log(stringrandomSongNoCommas);
 
+
 var answerArray = []; //Create an array of blanks for the random song
 	for (i = 0; i < stringrandomSongNoCommas.length; i++) {
 		answerArray[i] = "_";
@@ -25,7 +29,10 @@ var answerArray = []; //Create an array of blanks for the random song
 	}
 	//console.log(newAnswerArray);
 
+
 var remainingLetters = arrayrandomSong.length; //Number of letters remaining that need to be guessed
+
+}
 
 var guess = event.key; //Define a variable to capture the user's guess
 
@@ -33,6 +40,33 @@ var guess = event.key; //Define a variable to capture the user's guess
 
 //Start the game when user presses START
 document.getElementById("startButton").addEventListener("click", function() {
+			var name //user's name
+		var NumberofGuesses = 10; //Number of times a user can guess a letter
+		var lettersGuessed = []; //array to hold the letters already guessed
+		var wins = []; //Number of times the user has won
+
+		var songs = ["clique", "dirt off your shoulder", "drunk in love", "empire state of mind", "99 problems", "izzo", "bonnie and clyde"]; //Define an array of songs
+
+		var randomSong = songs[Math.floor(Math.random() * songs.length)]; //Pick a random song to use in Hangman
+
+		var arrayrandomSong = randomSong.split(""); //Take random song and split up into individual letters
+		console.log(arrayrandomSong);
+
+		var stringrandomSongNoCommas = arrayrandomSong.join(""); //Take random song and remove the commas
+		console.log(stringrandomSongNoCommas);
+
+		var answerArray = []; //Create an array of blanks for the random song
+			for (i = 0; i < stringrandomSongNoCommas.length; i++) {
+				answerArray[i] = "_";
+				console.log(answerArray);
+				//var newAnswerArray = answerArray.join(" ");
+				}
+				//console.log(newAnswerArray);
+
+		var remainingLetters = arrayrandomSong.length; //Number of letters remaining that need to be guessed
+
+		var guess = event.key; //Define a variable to capture the user's guess
+
 			//Ask the user's name
 			var name = prompt("Hi there, what's your name?");
 			//Greet the user and give instructions
@@ -46,25 +80,32 @@ document.getElementById("startButton").addEventListener("click", function() {
 			document.getElementById("blanks").innerHTML = answerArray.join(" ");
 
 			//This code captures the keypress and prints it out on the screen
-			document.onkeypress = function(keyPressed) {
-  				var keyPressed = keyPressed || window.event,
-    				charCode = keyPressed.keyCode || keyPressed.which,
-   					lettersGuessed = String.fromCharCode(charCode);
+			createHangmanSong(songs);
+			document.onkeypress = function(event) {
+  				var guess = event.key;
+  				var answerArray = stringrandomSongNoCommas.map(i => '_');
+  				
+  				var i = 0;
+
+				while (arrayrandomSong.indexOf(guess, i) != -1) {
+					//replace letter in blank array[]
+					answerArray.splice(i, 1, guess);
+
+					i++;
+
+				}
+
 			}
 
-			//Get a user's guess
-			//document.onkeypress = function(event) {
-			//	var guess = event.key;
+			//for (i = 0; i < arrayrandomSong; i++) {
 
-			for (i = 0; i < arrayrandomSong; i++) {
-
-					if (guess == arrayrandomSong[i]) {
-						guess == answerArray[i];
-						guess == lettersGuessed[i];
-						remainingLetters --;
-						document.getElementById("blanks").innerHTML = answerArray.join(" ");
-						document.getElementById("lettersRemaining").innerHTML = "Number of reamaining letters: " +remainingLetters;
-					}
+			//		if (guess == arrayrandomSong[i]) {
+			//			guess == answerArray[i];
+			//			guess == lettersGuessed[i];
+			//			remainingLetters --;
+			//			document.getElementById("blanks").innerHTML = answerArray.join(" ");
+			//			document.getElementById("lettersRemaining").innerHTML = "Number of reamaining letters: " +remainingLetters;
+			//		}
 
 					//else (guess !== arrayrandomSong[i]) {
 					//	alert("Wrong letter!"); //alert the user that the letter is wrong
@@ -73,7 +114,7 @@ document.getElementById("startButton").addEventListener("click", function() {
 					//}
 
 					document.getElementById("guessed").innerHTML = "Letters Guessed: " + lettersGuessed.join(" ");
-			}
+			//}
 
 			//	}
 
